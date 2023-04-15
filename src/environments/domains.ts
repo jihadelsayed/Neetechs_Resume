f1ddcf2fddf092b0804596f842591569f0dc6b76
Jihad El Sayed <david.pales1@gmail.com>
2023-04-15 14:41:08 +0200

    update

--- src/environments/domains.ts
+++ src/environments/domains.ts
@@ -6,10 +6,6 @@ export interface Domain {
 export const domains: Domain[] = [
   {
     name: "jihad",
-    hostname: "jihad.neetechs.com"
-  },
-  {
-    name: "jihad",
     hostname: "localhost"
   },
   {
@@ -21,7 +17,39 @@ export const domains: Domain[] = [
     hostname: "jihad.neetechs.com"
   },
   {
+    name: "gehad",
+    hostname: "gehad.neetechs.com"
+  },
+  {
     name: "retta",
     hostname: "retta.neetechs.com"
+  },
+  {
+    name: "retta",
+    hostname: "retta-elsayed.neetechs.com"
+  },
+  {
+    name: "maged",
+    hostname: "maged.neetechs.com"
+  },
+  {
+    name: "majd",
+    hostname: "majd.neetechs.com"
+  },
+  {
+    name: "gody",
+    hostname: "gody.neetechs.com"
+  },
+  {
+    name: "judy",
+    hostname: "judy.neetechs.com"
+  },
+  {
+    name: "amal",
+    hostname: "amal.neetechs.com"
+  },
+  {
+    name: "mahmoud",
+    hostname: "mahmoud.neetechs.com"
   }
 ];
